d5ce3db44c25e3f8f54a5b3e2bd562bb9796c1cc
naga169 <19421311+naga169@users.noreply.github.com>
2021-09-09 20:52:57 +0530

    Removed logs

--- src/components/PageNumbers.tsx
+++ src/components/PageNumbers.tsx
@@ -7,9 +7,7 @@ function PageNumbers(props: (any & number)) {
     const [lastIndex, setlastIndex] = useState(10);
     const [pageNumber, setPageNumber] = useState(1);
     const [pageNumberArray, setPageNumberArray] = useState([...Array(11)].map((_, idx) => idx).filter(e => e !== 0))
-    console.log(productsLength);
     const pageNumbersLimit = Math.ceil(productsLength / 12);
-    console.log(pageNumbersLimit);
     const onPageChange = (index: number) => {
         setPageNumber(index);
         onPageNumberChange(index);
@@ -17,7 +15,6 @@ function PageNumbers(props: (any & number)) {
     const getRange = (start: number, end: number) => {
         const startIndex = start === 0 ? 1 : start;
         const endIndex = end === productsLength ? end - 1 : end;
-
         return [...Array(endIndex - startIndex + 1)].map((_, idx) => start + idx).filter(e => e !== 0)
     }
 
@@ -28,25 +25,25 @@ function PageNumbers(props: (any & number)) {
             setFirstIndex(firstIndex + 1);
             setPageNumberArray(getRange(firstIndex + 1, lastIndex + 1));
         } else {
-            setPageNumberArray(getRange(firstIndex, 10));
+            setPageNumberArray(getRange(firstIndex, lastIndex));
         }
         onPageNumberChange(index);
     }
 
     const onPrevious = (index: number) => {
         setPageNumber(index);
-        if (index + 1 === lastIndex) {
+        if (index + 1 === firstIndex) {
             setlastIndex(lastIndex - 1);
             setFirstIndex(firstIndex - 1 === 0 ? 1 : firstIndex - 1);
             setPageNumberArray(getRange(firstIndex - 1 === 0 ? 1 : firstIndex - 1, lastIndex - 1));
         } else {
-            setPageNumberArray(getRange(1, 10));
+            setPageNumberArray(getRange(firstIndex, lastIndex));
         }
         onPageNumberChange(index);
     }
 
     return (
-        <nav className = "pageNav" aria-label="Page navigation example">
+        <nav className="pageNav" aria-label="Page navigation example">
             <ul className="pagination justify-content-end">
                 <li className={`page-item  ${pageNumber === 1 ? 'disabled' : ''}`}>
                     <a className="page-link" aria-disabled="false" onClick={() => onPrevious(pageNumber - 1)}>Previous</a>
@@ -54,12 +51,11 @@ function PageNumbers(props: (any & number)) {
                 {[...pageNumberArray].map(index => (
                     <li className={`page-item ${index === pageNumber ? 'active' : ''}`} onClick={() => onPageChange(index)}><a className="page-link">{index}</a></li>
                 ))}
-                <li className="page-item">
+                <li className={`page-item  ${pageNumber === pageNumbersLimit ? 'disabled' : ''}`}>
                     <a className="page-link" onClick={() => onNext(pageNumber + 1)}>Next</a>
                 </li>
             </ul>
         </nav>
-
     )
 }
 
